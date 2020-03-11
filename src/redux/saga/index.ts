e10232c13b36c858103ab31176a2ab337f690977
RuiHirano <r.hrn.0930@gmail.com>
2020-03-11 22:31:08 +0900

    　remove saga library

--- src/redux/saga/index.ts
+++
@@ -1,9 +0,0 @@
-import { all } from 'redux-saga/effects'
-/*import signSaga from './sign'
-import itemSaga from './item'
-import userSaga from './user'
-
-export default function* rootSaga() {
-	yield all([signSaga(), itemSaga(), userSaga()])
-}
-*/
--- src/redux/saga/selector.ts
+++
@@ -1,8 +0,0 @@
-
-import { ReduxState } from '../module'
-
-export const getItems = (state: ReduxState) => state.Items
-export const getUser = (state: ReduxState) => state.User
-export const getApp = (state: ReduxState) => state.App
-export const getAppState = (state: ReduxState) => state.App.State
-export const getIsSignIn = (state: ReduxState) => state.App.Loading.IsLoading
