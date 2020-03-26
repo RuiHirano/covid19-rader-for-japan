9dd2f2c149e1fd6158c7b83b4053f5bb0e2d1db7
RuiHirano <r.hrn.0930@gmail.com>
2020-03-26 19:55:27 +0900

    get file from s3-aws

--- backend/go.mod
+++ backend/go.mod
@@ -3,6 +3,8 @@ module main
 go 1.13
 
 require (
+	github.com/aws/aws-lambda-go v1.15.0 // indirect
+	github.com/aws/aws-sdk-go v1.29.32 // indirect
 	github.com/dgrijalva/jwt-go v3.2.0+incompatible // indirect
 	github.com/valyala/fasttemplate v1.1.0 // indirect
 	handler v0.0.0-00010101000000-000000000000 // indirect
--- frontend/src/api/index.ts
+++ frontend/src/api/index.ts
@@ -2,101 +2,66 @@ import { Patient, Sex } from "../types"
 import { prefectures } from "../data/prefecture"
 import moment from "moment"
 import axios from "axios"
+var aws = require('aws-sdk')
+const accessKeyId = process.env.REACT_APP_AWS_ACCESS_KEY
+const secretAccessKey = process.env.REACT_APP_AWS_SECRET_ACCESS_KEY
+aws.config.update({
+	region: 'ap-northeast-1',
+	credentials: new aws.Credentials(accessKeyId, secretAccessKey)
+});
+var s3 = new aws.S3();
 
-export class GoogleSheetsAPI {
+export class API {
 	private baseURL: string
 
 	constructor(url: string = 'http://localhost:5000') {
 		this.baseURL = url;
 	}
 
-	getBaseURL() {
-		return this.baseURL;
-	}
-
-	setBaseURL(val: string) {
-		this.baseURL = val;
-	}
-
-	async get({
-		url = "",
-		params = {},
-	}) {
-		return axios(
-			{
-				url: url,
-				method: 'get',
-				baseURL: this.baseURL,
-				params: params,
-			}
-		);
-	}
-
 	async getPatients() {
+		var params = {
+			Bucket: "covid19.dataset",
+			Key: 'patient-dataset.csv'
+		};
+		const data = await s3.getObject(params, function (err: any, data: any) { return data }).promise();
 
-		return this.get({
-			url: '/file/patient',
-		});
+		const patients: Patient[] = []
+		const str = new TextDecoder().decode(data.Body)
+		var tmp = str.replace("\n", "").split("\r"); // 改行を区切り文字として行を要素とした配列を生成
+		tmp.forEach((tmpstr: string, index: number) => {
+			const tmp2 = tmpstr.replace("\n", "").split(",")
+			if (tmp2.length === 25 && index !== 0) {
+				patients.push({
+					ID: tmp2[0],
+					Residence: tmp2[13],
+					Age: tmp2[16].indexOf("代") != -1 ? parseInt(tmp2[16].split("代")[0]) : -1,
+					Sex: tmp2[17] === "男性" ? Sex.MALE : Sex.FEMALE,
+					Occupation: tmp2[18],
+					Prefecture: {
+						ID: tmp2[1],
+						Name: tmp2[10],
+						Latitude: parseFloat(tmp2[11]),
+						Longitude: parseFloat(tmp2[12]),
+					},
+					ActionHistory: tmp2[4],
+					SymptomHistory: tmp2[3],
+					FeverDate: moment(tmp2[5]),
+					ConsultationDate: moment(tmp2[6]),
+					PublicationDate: moment(tmp2[7]),
+					RecoveryDate: moment(tmp2[8]),
+					OverseasTravelFlag: tmp2[19] === "1" ? true : false,
+					OverseasTravelName: tmp2[20],
+					CloseContact: tmp2[23],
+					Source: tmp2[24],
+				})
+			}
+		})
+		return patients
 	}
+
+
 
 
 
 }
 
-
-const mockPatients: Patient[] = [
-	{
-		ID: "0",
-		Residence: "北海道",
-		Age: 20,
-		Sex: Sex.FEMALE,
-		Occupation: "医者",
-		Prefecture: prefectures[0],
-		ActionHistory: "", // 行動歴
-		SymptomHistory: "",	// 症状・経過
-		FeverDate: moment(),  // 発熱観察日 
-		ConsultationDate: moment(), // 受診日
-		PublicationDate: moment(), // 公表日
-		RecoveryDate: moment(),  // 回復日
-		OverseasTravelFlag: true, // 海外渡航フラグ
-		OverseasTravelName: "China",  // 渡航先
-		CloseContact: "", // 濃厚接触者情報
-		Source: "http://"  // 情報源
-	},
-	{
-		ID: "1",
-		Residence: "愛知県",
-		Age: 20,
-		Sex: Sex.FEMALE,
-		Occupation: "医者",
-		Prefecture: prefectures[0],
-		ActionHistory: "", // 行動歴
-		SymptomHistory: "",	// 症状・経過
-		FeverDate: moment(),  // 発熱観察日 
-		ConsultationDate: moment(), // 受診日
-		PublicationDate: moment(), // 公表日
-		RecoveryDate: moment(),  // 回復日
-		OverseasTravelFlag: true, // 海外渡航フラグ
-		OverseasTravelName: "China",  // 渡航先
-		CloseContact: "", // 濃厚接触者情報
-		Source: "http://"  // 情報源
-	},
-	{
-		ID: "2",
-		Residence: "東京都",
-		Age: 20,
-		Sex: Sex.FEMALE,
-		Occupation: "医者",
-		Prefecture: prefectures[0],
-		ActionHistory: "", // 行動歴
-		SymptomHistory: "",	// 症状・経過
-		FeverDate: moment(),  // 発熱観察日 
-		ConsultationDate: moment(), // 受診日
-		PublicationDate: moment(), // 公表日
-		RecoveryDate: moment(),  // 回復日
-		OverseasTravelFlag: true, // 海外渡航フラグ
-		OverseasTravelName: "China",  // 渡航先
-		CloseContact: "", // 濃厚接触者情報
-		Source: "http://"  // 情報源
-	},
-]
